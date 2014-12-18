eb3d906a5486923feaa5ce633af13355107dbf29
adi <aditya@eddywebs.com>
2014-12-17 19:55:32 -0500

    missing config file

---
+++ src/Eddywebs/DbToApi/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Example configuration. All fields optional except the dataset name.
+*/
+
+$args = $config;
+
+// $args = array( 
+//             'name' => null,
+//             'username' => 'root',
+//             'password' => 'root',
+//             'server' => 'localhost',
+//             'port' => 3306,
+//             'type' => 'mysql',
+//             'table_blacklist' => array(),
+//             'column_blacklist' => array(),
+// );
+
+register_db_api( $config['name'], $args );
